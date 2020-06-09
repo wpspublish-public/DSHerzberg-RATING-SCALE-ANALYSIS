09b852945f91f237d1f0db40874e588a6e03cc93
David Herzberg <dherzberg@wpspublish.com>
2020-06-08 18:03:05 -0700

    finalizing descriptives code for documentation

--- CODE/descriptives-RSA.R
+++ CODE/descriptives-RSA.R
@@ -2,9 +2,17 @@ suppressMessages(library(here))
 suppressMessages(suppressWarnings(library(tidyverse)))
 suppressMessages(library(psych))
 
-# read data
-data_input_sim <- suppressMessages(read_csv(here("INPUT-FILES/data-input-sim.csv")))
-data_input_bfi <- suppressMessages(read_csv(here("INPUT-FILES/data-input-bfi.csv")))
+# read data from remote URL
+urlRemote_path  <- "https://raw.githubusercontent.com/"
+github_path <- "DSHerzberg/RATING-SCALE-ANALYSIS/master/INPUT-FILES/"
+
+data_input_sim <- suppressMessages(read_csv(url(
+  str_c(urlRemote_path, github_path, "data-input-sim.csv")
+)))
+data_input_bfi <- suppressMessages(read_csv(url(
+  str_c(urlRemote_path, github_path, "data-input-bfi.csv")
+)))
+
 
 # extract elements unique to each data set: name suffix, item cols, item cats
 data_name_suffix <- c("sim", "bfi")
@@ -28,35 +36,39 @@ item_cats <- list(sim_item_cats, bfi_item_cats)
 # contained within str_c(). These objects are put into a list, to be one of the
 # inputs that pmap() iterates over. Now all three inputs to pmap() are lists
 # containing named objects
-freq_item_val_tables <- list(mget(str_c("data_input_", data_name_suffix)),
-                             item_cols,
-                             item_cats) %>%
+list(mget(str_c("data_input_", data_name_suffix)),
+     item_cols,
+     item_cats,
+     data_name_suffix) %>%
   # ..1 etc is how to refer to multiple inputs in pmap(), Here `..1` refers to
   # the list containing the input data frames for which we are getting freq
   # counts.
-  pmap(~ ..1 %>% 
-         # `..2` refers to `item_cols`, the list containing the char vecs that
-         # name the item columns in each input data set. Because these names are
-         # coming into a dplyr function as quoted names, they need to be
-         # unquoted with `!!`
-         select(!! ..2) %>% 
-         # `pivot_longer` is an updated version of `gather`
-         pivot_longer(everything(), names_to = 'var', values_to = 'value') %>% 
-         count(var, value) %>% 
-         # `pivot_wider` is an updated version of `spread`
-         pivot_wider(names_from = value, values_from = n) %>% 
-         arrange(match(var, !!..2)) %>%
-         # `..3` refers to `item_cats`, the list containing the char vecs that
-         # name the item response categories for each input data set.
-         select(var, !! ..3)
-  ) %>% 
+  pmap(
+    ~ ..1 %>%
+      # `..2` refers to `item_cols`, the list containing the char vecs that
+      # name the item columns in each input data set. Because these names are
+      # coming into a dplyr function as quoted names, they need to be
+      # unquoted with `!!`
+      select(!!..2) %>%
+      # `pivot_longer` is an updated version of `gather`
+      pivot_longer(everything(), names_to = 'var', values_to = 'value') %>%
+      count(var, value) %>%
+      # `pivot_wider` is an updated version of `spread`
+      pivot_wider(names_from = value, values_from = n) %>%
+      arrange(match(var,!!..2)) %>%
+      # `..3` refers to `item_cats`, the list containing the char vecs that
+      # name the item response categories for each input data set.
+      mutate(data = case_when(rownames(.) == "1" ~ ..4,
+                              T ~ NA_character_)) %>%
+      select(data, var,!!..3)
+  ) %>%
   # trying to reduce my use of `assign()` to place data objecds intop the global
   # environment. Let `pmap()` output a list of dfs `freq_item_val_tables`, then
   # use `set_names()` to give each df the desired name
-  set_names(str_c("freq_item_val_", data_name_suffix))
-
-# `list2env()` extracts the data frames from the list into the global environment
-list2env(freq_item_val_tables, envir=.GlobalEnv)
+  set_names(str_c("freq_item_val_", data_name_suffix)) %>%
+  
+  # `list2env()` extracts the data frames from the list into the global environment
+  list2env(envir = .GlobalEnv)
 
 # demo tables
 var_order <- c("age_range", "gender", "educ", "ethnic", "region", "clin_status")
@@ -79,28 +91,51 @@ cat_order <- c(
   "never", "occasionally","frequently", "always"
 )
 
-freq_demos_tables <-
-  mget(str_c("data_input_", data_name_suffix)
-       ) %>%
-  map( ~ .x %>%
-          select(all_of(var_order)) %>% 
-  pivot_longer(everything(), names_to = 'var', values_to = 'cat') %>%
-  count(var, cat) %>%
-  arrange(match(var, var_order), match(cat, cat_order)) %>%
-  mutate(
-    var = case_when(
-      lag(var) == "age_range" & var == "age_range" ~ "",
-      lag(var) == "gender" & var == "gender" ~ "",
-      lag(var) == "educ" & var == "educ" ~ "",
-      lag(var) == "ethnic" & var == "ethnic" ~ "",
-      lag(var) == "region" & var == "region" ~ "",
-      lag(var) == "clin_status" & var == "clin_status" ~ "",
-      TRUE ~ var
-    )
-  )
-) %>% 
-  set_names(str_c("freq_demos_", data_name_suffix))
+list(mget(str_c("data_input_", data_name_suffix)),
+     data_name_suffix) %>%
+  pmap(
+    ~ ..1 %>%
+      select(all_of(var_order)) %>%
+      pivot_longer(everything(), names_to = 'var', values_to = 'cat') %>%
+      count(var, cat) %>%
+      arrange(match(var, var_order), match(cat, cat_order)) %>%
+      mutate(
+        var = case_when(
+          lag(var) == "age_range" & var == "age_range" ~ "",
+          lag(var) == "gender" & var == "gender" ~ "",
+          lag(var) == "educ" & var == "educ" ~ "",
+          lag(var) == "ethnic" & var == "ethnic" ~ "",
+          lag(var) == "region" & var == "region" ~ "",
+          lag(var) == "clin_status" & var == "clin_status" ~ "",
+          TRUE ~ var
+        ),
+        data = case_when(rownames(.) == "1" ~ ..2,
+                         T ~ NA_character_)
+      ) %>%
+      select(data, var, cat, n)
+  ) %>%
+  set_names(str_c("freq_demos_", data_name_suffix)) %>%
+  list2env(envir = .GlobalEnv)
 
-list2env(freq_demos_tables, envir=.GlobalEnv)
+# raw score descriptives tables
+list(mget(str_c("data_input_", data_name_suffix)),
+     data_name_suffix) %>%
+  pmap(
+    ~ ..1 %>%
+      select(contains('raw')) %>%
+      describe(fast = T) %>%
+      rownames_to_column() %>%
+      rename(scale = rowname) %>%
+      mutate(
+        data = case_when(rownames(.) == "1" ~ ..2,
+                         T ~ NA_character_),
+        across(c(mean, sd),
+               ~ round(., 2))
+      ) %>%
+      select(data, scale, n, mean, sd)
+  ) %>%
+  set_names(str_c("raw_desc_", data_name_suffix)) %>%
+  list2env(envir = .GlobalEnv)
+
 
 rm(list = setdiff(ls(), ls(pattern = "_bfi|_sim")))
