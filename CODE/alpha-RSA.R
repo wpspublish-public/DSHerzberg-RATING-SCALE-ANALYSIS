b5ee2535cfc4eaebea2eb8c5b70fe47a2268140e
David Herzberg <dherzberg@wpspublish.com>
2020-10-22 17:31:25 -0700

    building out robust code for RSA alpha

--- CODE/alpha-RSA.R
+++ CODE/alpha-RSA.R
@@ -21,12 +21,38 @@ data_RS_sim_teen_teacher <- suppressMessages(read_csv(url(
   str_c(urlRemote_path, github_path, "data-RS-sim-teen-teacher.csv")
 )))
 
+# Create a list containing scale item vectors for each form (needed for subscale alphas)
+
 form_acronyms <- c("cp", "ct", "tp", "tt")
 
+scale_items_suffix <- c("S1", "S2", "S3", "S4", "S5", "TOT")
 
-CPS1_items <- str_c("cpi", str_pad(as.character(seq(1, 50, by = 5)), 2, side = "left", pad = "0"))
-CPS2_items <- str_c("cpi", str_pad(as.character(seq(2, 50, by = 5)), 2, side = "left", pad = "0"))
-CPS3_items <- str_c("cpi", str_pad(as.character(seq(3, 50, by = 5)), 2, side = "left", pad = "0"))
-CPS4_items <- str_c("cpi", str_pad(as.character(seq(4, 50, by = 5)), 2, side = "left", pad = "0"))
-CPS5_items <- str_c("cpi", str_pad(as.character(seq(5, 50, by = 5)), 2, side = "left", pad = "0"))
-CPTOT_items <- str_c("cpi", str_pad(as.character(1:50), 2, side = "left", pad = "0"))
+S1_item_names <- function(x) {
+  str_c(str_c(x, "i"), str_pad(as.character(seq(1, 50, by = 5)), 2, side = "left", pad = "0"))
+}
+S2_item_names <- function(x) {
+  str_c(str_c(x, "i"), str_pad(as.character(seq(2, 50, by = 5)), 2, side = "left", pad = "0"))
+}
+S3_item_names <- function(x) {
+  str_c(str_c(x, "i"), str_pad(as.character(seq(3, 50, by = 5)), 2, side = "left", pad = "0"))
+}
+S4_item_names <- function(x) {
+  str_c(str_c(x, "i"), str_pad(as.character(seq(4, 50, by = 5)), 2, side = "left", pad = "0"))
+}
+S5_item_names <- function(x) {
+  str_c(str_c(x, "i"), str_pad(as.character(seq(5, 50, by = 5)), 2, side = "left", pad = "0"))
+}
+TOT_item_names <- function(x) {
+  str_c(str_c(x, "i"), str_pad(as.character(1:50), 2, side = "left", pad = "0")) 
+}
+
+list_item_names <- list(S1_item_names, S2_item_names, S3_item_names, 
+                        S4_item_names, S5_item_names, TOT_item_names)
+
+scale_item_vectors <- crossing(form_acronyms, scale_items_suffix) %>% 
+  mutate(scale_items = str_c(str_to_upper(form_acronyms), scale_items_suffix, "_items"),
+         fun_item_names = rep(list_item_names, 4), 
+         item_names = invoke_map(fun_item_names, form_acronyms)) %>% 
+  pull(item_names, scale_items) 
+
+# Create list of dfs containing subscale item scores for all persons
--- CODE/scratch.R
+++ CODE/scratch.R
@@ -1,35 +1,23 @@
-form_acronyms <- c("cp", "ct", "tp", "tt")
-
-scale_items_suffix <- c("S1", "S2", "S3", "S4", "S5", "TOT")
-
-
-S1_item_names <- function(x) {
-  str_c(str_c(x, "i"), str_pad(as.character(seq(1, 50, by = 5)), 2, side = "left", pad = "0"))
-}
-S2_item_names <- function(x) {
-  str_c(str_c(x, "i"), str_pad(as.character(seq(2, 50, by = 5)), 2, side = "left", pad = "0"))
-}
-S3_item_names <- function(x) {
-  str_c(str_c(x, "i"), str_pad(as.character(seq(3, 50, by = 5)), 2, side = "left", pad = "0"))
-}
-S4_item_names <- function(x) {
-  str_c(str_c(x, "i"), str_pad(as.character(seq(4, 50, by = 5)), 2, side = "left", pad = "0"))
-}
-S5_item_names <- function(x) {
-  str_c(str_c(x, "i"), str_pad(as.character(seq(5, 50, by = 5)), 2, side = "left", pad = "0"))
-}
-TOT_item_names <- function(x) {
-  str_c(str_c(x, "i"), str_pad(as.character(1:50), 2, side = "left", pad = "0")) 
-}
-
-list_item_names <- list(S1_item_names, S2_item_names, S3_item_names, 
-                        S4_item_names, S5_item_names, TOT_item_names)
+map_df(scale_order,
+       ~
+         Child_512_Home %>%
+         select(eval(as.name(
+           str_c(.x, '_items_Child_512_Home')
+         ))) %>%
+         assign(str_c(.x, '_item_scores_512'), ., envir = .GlobalEnv))
 
 
-temp1 <- crossing(form_acronyms, scale_items_suffix) %>% 
-  mutate(scale_items = str_c(str_to_upper(form_acronyms), scale_items_suffix, "_items")) %>% 
-  mutate(fun_item_names = rep(list_item_names, 4)) %>% 
-  mutate(item_names = map(form_acronyms, fun_item_names))
-
-map( ~
-       tibble(var = map(var_order, ~ .y %>% filter(var == .x), .y = .x))) 
+temp_list1 <- lst(
+  data_RS_sim_child_parent,
+  data_RS_sim_child_teacher,
+  data_RS_sim_teen_parent,
+  data_RS_sim_teen_teacher
+) %>% 
+  map( ~
+       map(scale_item_vectors, ~ .y %>% select(all_of(.x)), .y = .x)) 
+  
+temp_list2 <- map(scale_item_vectors[1:6],
+                  ~
+                    data_RS_sim_child_parent %>%
+                    select(all_of(.x))
+)
