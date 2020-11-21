503fa2a5b95be291cd1f2f18fc748d0cb461747f
David Herzberg <dherzberg@wpspublish.com>
2020-11-20 16:59:28 -0800

    final draft of dupID procedure documentation

--- CODE/dupID-check-RSA.R
+++ CODE/dupID-check-RSA.R
@@ -1,5 +1,3 @@
-# Load packages, read data
-
 suppressMessages(library(here))
 suppressMessages(suppressWarnings(library(tidyverse)))
 
@@ -10,27 +8,23 @@ data_RS_sim_child_parent <- suppressMessages(read_csv(url(
   str_c(urlRemote_path, github_path, "data-RS-sim-child-parent.csv")
 )))
 
-# recode four cases to have missing ID numbers, four DUP IDS
-miss_dup <- data_RS_sim_child_parent %>%
-  mutate(across(ID,
-                ~
-                  case_when(
-                    row_number() %in% c(10, 35, 579, 991) ~ lag(.x),
-                    row_number() %in% c(1, 208, 414, 793) ~ NA_real_,
-                    T ~ .x
-                  )))
-
-# returns row number of FIRST dup ID encountered, or returns 0 if no dups
 anyDuplicated(miss_dup$ID)
-
-# Check for any NAs on IDNumber, returns TRUE if NA exist
 any(is.na(miss_dup$ID))
 
-# extract cases with Dup ID numbers or NA on IDNumber, write out for investigation
-data_RS_sim_child_parent_dupMissIDs <- miss_dup %>% 
-  mutate(dup = duplicated(ID)) %>% 
-  filter(dup == TRUE | is.na(ID)) %>% 
-  select(-dup) 
+data_RS_sim_child_parent_dupMissIDs <- miss_dup %>%
+  arrange(ID) %>% 
+  mutate(dup = duplicated(ID)) %>%
+  filter(dup == T | lead(dup) == T | is.na(ID)) %>%
+  select(-dup)
 
-# NEXT DO YOU WANT SEPARATE REPORTS FOR DUP AND MISSING, DO YOU WANT THE PAIR OF
-# CASES FOR EACH DUP ID?
+write_csv(data_RS_sim_child_parent_dupMissIDs,
+  here(
+    str_c(
+      'OUTPUT-FILES/DUP-IDS/data-RS-sim-child-parent-dupIDs-missingIDs-',
+      format(Sys.Date(), "%Y-%m-%d"),
+      '.csv'
+    )
+  ), 
+  na = 'missing'
+)
+
--- CODE/scratch.R
+++ CODE/scratch.R
@@ -1,26 +1,9 @@
-alpha_output <- scale_item_data %>%
-  mutate(alpha = map(items, ~ alpha(cor(.x))[["total"]])) %>%
-  unnest(alpha) %>%
-  select(form, scale, raw_alpha) %>%
-  rename(alpha = raw_alpha) %>%
-  left_join(scale_n_mean_sd, by = c("form", "scale")) %>%
-  group_by(form) %>%
-  mutate(
-    SEM = sd * (sqrt(1 - alpha)),
-    CV_90_UB = round(mean + 1.6449 * SEM),
-    CV_90_LB = round(mean - 1.6449 * SEM),
-    CV_95_UB = round(mean + 1.96 * SEM),
-    CV_95_LB = round(mean - 1.96 * SEM),
-    CI_90 = str_c(CV_90_LB, "--", CV_90_UB),
-    CI_95 = str_c(CV_95_LB, "--", CV_95_UB),
-    across(is.numeric, ~ round(., 2)),
-    form = case_when(row_number() == 1 ~ form,
-                     T ~ NA_character_),
-    n = case_when(row_number() == 1 ~ n,
-                  T ~ NA_real_)
-  ) %>%
-  select(form, n, scale, alpha, SEM, CI_90, CI_95) 
-
-write_csv(alpha_output,
-          here("OUTPUT-FILES/TABLES/alpha-summary-by-form.csv"),
-          na = "")
+miss_dup <- data_RS_sim_child_parent %>%
+  mutate(across(ID,
+                ~
+                  case_when(
+                    row_number() %in% c(10, 35, 579, 991) ~ lag(.x),
+                    row_number() %in% c(11) ~ lag(lag(.x)),
+                    row_number() %in% c(1, 2, 414, 793) ~ NA_real_,
+                    T ~ .x
+                  )))
