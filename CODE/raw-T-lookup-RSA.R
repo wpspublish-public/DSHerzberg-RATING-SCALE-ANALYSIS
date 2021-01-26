76b981222e9625dcf3fffbfe7c7c12398bdfddb2
David Herzberg <dherzberg@wpspublish.com>
2021-01-25 17:19:56 -0800

    document complete()

--- CODE/raw-T-lookup-RSA.R
+++ CODE/raw-T-lookup-RSA.R
@@ -116,16 +116,6 @@ get(str_c("data", age_range_name, form_name, "nt", sep = "_")) %>%
 
 # GENERATE RAW-TO-T LOOKUP TABLES -----------------------------------------
 
-# Generate raw-to-T lookup columns. Input is stand sample with raw scores and
-# normalized T scores for each case. map() generates a list of two-col lookup
-# tables, one for each scale. Each 2-col lookup has raw col and nt (t-score)
-# col. Group cases by raw score, relationship between raw and T is many-to-one.
-#
-# map() iterates over scale_suffix, the char vec containing the six strings that
-# identify (differentiate) the six scale names. The names themselves are
-# concatenated with a static prefix. the .x argument of map(), and a static
-# score type suffix (e.g., "_raw"). !!sym() is used to unquote the names (which
-# are strings) so they can be arguments in dplyr functions.
 all_lookup_basic <- map(
   scale_suffix,
   ~ get(str_c(
@@ -134,12 +124,6 @@ all_lookup_basic <- map(
     group_by(!!sym(str_c(
       scale_prefix, .x, "_raw"
     ))) %>%
-    # Because raw-to-T is many to one, each value of raw has one and only one
-    # value of T, and thus summarizing by the min value of T per raw returns the
-    # ONLY value of T per raw. But we need the raw column to contain all
-    # possible values of raw, and not all possible values of raw are represented
-    # in the stand sample. Thus current data object jumps possible raw values
-    # (e.g, raw = 62 and raw = 65 might be adjacent rows in this table)
     summarize(!!sym(str_c(
       scale_prefix, .x, "_nt"
     )) := min(!!sym(
