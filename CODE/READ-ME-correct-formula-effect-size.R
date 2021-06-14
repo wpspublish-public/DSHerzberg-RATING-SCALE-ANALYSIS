a94fbc4c77b58c720da33fe05bb1da2bc9423506
David Herzberg <dherzberg@wpspublish.com>
2021-06-14 16:42:54 -0700

    corrected

--- CODE/READ-ME-correct-formula-effect-size.R
+++ CODE/READ-ME-correct-formula-effect-size.R
@@ -1,3 +1,3 @@
 # Correct formulat for Cohen's d (effect size)
 
-ES_new = abs((mean_typ - mean_clin) / sqrt((n_clin*(sd_clin^2) + n_typ*(sd_typ^2)) / (n_clin + n_typ)))
+ES_new = abs((mean_typ - mean_clin) / sqrt(((n_clin*(sd_clin^2)) + (n_typ*(sd_typ^2))) / (n_clin + n_typ))),
