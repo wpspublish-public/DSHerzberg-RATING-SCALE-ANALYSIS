cfdc96341dee5a06724f2b0e59a09887fa10c032
David Herzberg <dherzberg@wpspublish.com>
2021-07-07 17:59:19 -0700

    experiment with chi-sq test for contingency tables

--- CODE/scratch.R
+++ CODE/scratch.R
@@ -1,14 +1,5 @@
-temp2 <- data_child_parent  %>%
-  select(all_of(var_order)) %>%
-  pivot_longer(everything(), names_to = "variable", values_to = "category") %>%
-  group_by(variable, category) %>%
-  count(variable, category) %>%
-  arrange(match(variable, var_order), match(category, cat_order)) %>%
-  ungroup() %>%
-  mutate(across(
-    variable,
-    ~
-      case_when(lag(.x) == .x ~ NA_character_,
-                T ~ .x)
-  ))
-
+set.seed(1234)
+sample_60perc2 <- sample_full %>% 
+  group_by(age, gender, educ, ethnic, region) %>%
+  sample_frac(0.6) %>% 
+  ungroup()
