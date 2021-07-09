fb98b115f21f7e435de5e8ef2fdb372ea6860a6e
David Herzberg <dherzberg@wpspublish.com>
2021-07-08 18:02:00 -0700

    use splitstackshape::stratified to do sub-sampling within grouping structure

--- CODE/scratch.R
+++ CODE/scratch.R
@@ -1,5 +1,14 @@
 set.seed(1234)
 sample_60perc2 <- sample_full %>% 
-  group_by(age, gender, educ, ethnic, region) %>%
-  sample_frac(0.6) %>% 
+  # group_by(age, gender, educ, ethnic, region) %>%
+  slice_sample(prop = .6) %>% 
   ungroup()
+
+# try splitstackshape::stratified()
+
+library(splitstackshape)
+
+set.seed(1234)
+temp1 <- stratified(sample_full,
+                    c("age", "gender", "educ", "ethnic", "region"),
+                    size = .6)
